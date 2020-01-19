b946d643d186e702e43d46f46c01d10c1c3417d8
Travis CI <travis@travis-ci.org>
2020-01-19 15:48:02 +0000

    'Deploy project with Ts.ED v5.39.0'

---
+++ src/Server.ts
@@ -0,0 +1,38 @@
+import {GlobalAcceptMimesMiddleware, ServerLoader, ServerSettings} from "@tsed/common";
+import "@tsed/mongoose";
+import "@tsed/swagger";
+import * as bodyParser from "body-parser";
+import * as compress from "compression";
+import * as cookieParser from "cookie-parser";
+import * as methodOverride from "method-override";
+
+@ServerSettings({
+  rootDir: __dirname,
+  acceptMimes: ["application/json"],
+  port: process.env.PORT || 8000,
+  httpsPort: false,
+  passport: {},
+  mongoose: {
+    url: process.env.mongoose_url || "mongodb://127.0.0.1:27017/example-mongoose-test"
+  },
+  swagger: {
+    path: "/api-docs"
+  },
+  debug: false
+})
+export class Server extends ServerLoader {
+  $beforeRoutesInit(): void | Promise<any> {
+
+    this
+      .use(GlobalAcceptMimesMiddleware)
+      .use(cookieParser())
+      .use(compress({}))
+      .use(methodOverride())
+      .use(bodyParser.json())
+      .use(bodyParser.urlencoded({
+        extended: true
+      }));
+
+    return null;
+  }
+}
---
+++ test/integrations/calendars.spec.ts
@@ -0,0 +1,26 @@
+import {ExpressApplication} from "@tsed/common";
+import {TestContext} from "@tsed/testing";
+import {TestMongooseContext} from "@tsed/testing-mongoose";
+import {expect} from "chai";
+import * as SuperTest from "supertest";
+import {Server} from "../../src/Server";
+
+describe("Calendars", () => {
+  let request: SuperTest.SuperTest<SuperTest.Test>;
+  // bootstrap your expressApplication in first
+  before(TestMongooseContext.bootstrap(Server));
+  before(TestMongooseContext.inject([ExpressApplication], (expressApplication: ExpressApplication) => {
+    request = SuperTest(expressApplication);
+  }));
+  after(TestContext.reset);
+
+  // then run your test
+  describe("GET /rest/calendars", () => {
+    it("should return all calendars", async () => {
+      const response = await request.get("/rest/calendars").expect(200);
+      let obj = JSON.parse(response.text);
+
+      expect(obj).to.be.an("array");
+    });
+  });
+});
